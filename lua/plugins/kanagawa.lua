19a208ba564ef3dc9e0c047f9d282857e064d156
doorknob88 <self@doorknob88.me>
2024-05-03 10:38:24 -0700

    removed flutter and consolidated themes into a themes plugin fiel

--- lua/plugins/kanagawa.lua
+++
@@ -1,5 +0,0 @@
-return {
-  { "rebelot/kanagawa.nvim" },
-  -- vim.cmd("colorscheme kanagawa-wave")
-  -- vim.cmd("colorscheme kanagawa-dragon")
-}
---
+++ lua/plugins/themes.lua
@@ -0,0 +1,7 @@
+return {
+  {
+    "rose-pine/neovim", -- rose pine theme
+    "rebelot/kanagawa.nvim", -- kanagawa theme
+    "craftzdog/solarized-osaka.nvim", -- osaka theme
+  },
+}
