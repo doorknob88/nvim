4afae7db5e584f36aa43860310012f93fe780d4c
doorknob88 <github@doorknob88.me>
2025-12-23 14:09:05 -0800

    updated golang snacks to not show too much stuff

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,14 @@
+-- lazy.nvim
+return {
+  {
+    "folke/snacks.nvim",
+    ---@type snacks.Config
+    opts = {
+      gh = {
+        -- your gh configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      },
+    },
+  },
+}
