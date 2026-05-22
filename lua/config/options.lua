97d8baee275ed32eaf9767465720530ef3cb24ba
doorknob88 <doorknob88@users.noreply.github.com>
2026-05-22 12:46:32 -0700

    disabled relative line numbers and disabled autoformat for yaml

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,5 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.autoformat = false -- manual: <leader>cf, toggle: <leader>uf
+vim.g.autoformat = true -- toggle: <leader>uf, manual: <leader>cf. Disabled per-filetype in autocmds.lua
+vim.opt.relativenumber = false
